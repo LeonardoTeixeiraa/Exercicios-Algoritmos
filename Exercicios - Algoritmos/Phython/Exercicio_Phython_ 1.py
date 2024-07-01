4279722b26d4c0377469f92f5b392bf3ef7d518b
Leonardo Teixeira <167580608+LeonardoTeixeiraa@users.noreply.github.com>
2024-07-01 09:50:12 -0300

    Update Exercicio_Phython_ 1.py

--- Exercicios - Algoritmos/Phython/Exercicio_Phython_ 1.py
+++ Exercicios - Algoritmos/Phython/Exercicio_Phython_ 1.py
@@ -1,9 +1,7 @@
-'''
-Faça um programa que receba o salário base de um funcionário.
-Calcule e mostre o salário a receber, sabendo que esse funcionário tem gratificação
-de R$ 50,00 e paga imposto de 10% sobre o salário base.
 
-'''
+# Faça um programa que receba o salário base de um funcionário.
+# Calcule e mostre o salário a receber, sabendo que esse funcionário tem gratificação
+# de R$ 50,00 e paga imposto de 10% sobre o salário base.
 
 #variaveis
 sal=0.0 
@@ -11,7 +9,7 @@ sal_f=0.0
 desc=0.0
 #sal, sal_f, desc = 0.0,0.0,0.0
 
-#programa
+#código
 sal = float(input("Informe o salário do funcionário: "))
 
 desc = sal * (10/100)
